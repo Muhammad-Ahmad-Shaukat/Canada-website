8fc5164bbd7da4acb8beee3ec46e1d226869bff2
Muhammad-Ahmad-Shaukat <ahmadshaukat328@gmail.com>
2025-08-23 08:55:54 -0400

    homepage content chnage

--- app/page.jsx
+++ app/page.jsx
@@ -211,7 +211,7 @@ export default function Home() {
       <div className="w-full text-center my-6 px-6">
         <div className="border-t-5 border-[#0f3b52]"></div>
           <p className="bounded-jazan-txt text-[#0f3b52] font-bold text-2xl py-3 text-left leading-[1.2]">
-            Jazan, Saudia Arabia is a great place to expand or relocate your business in MENA region.
+            Jazan offers more than opportunity. It offers certainty. With strategic oversight, projects across energy, petrochemicals, logistics, tourism, agriculture, manufacturing, and renewables are advancing on clear timelines. Global players from Europe, Asia, and the United States are already active. The question for Canada is not if Jazan should be on the map, but how quickly to enter.
           </p>
         <div className="border-t-5 border-[#0f3b52]"></div>
       </div>
